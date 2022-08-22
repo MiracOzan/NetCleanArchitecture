309303ac1cec29f9faac299d2eede14578752639
Miraç Ozan T <m.ozan.tosun@gmail.com>
2022-08-22 20:14:42 +0300

    Refactor

--- Application/Services/Repositories/IBrandRepository.cs
+++ Application/Services/Repositories/IBrandRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+using Domain.Entities;
 
 namespace Application.Services.Repositories
 {
-    public interface IBrandRepository : IAsyncRepository<Brand>, IRepository<Brand>
+    public interface IBrandRepository : IRepository<Brand>
     {
 
     }
--- Persistence/Contexts/BaseDbContext.cs
+++ Persistence/Contexts/BaseDbContext.cs
@@ -21,15 +21,7 @@ namespace Persistence.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Brand>(a =>
-            {
-                a.ToTable("Brands").HasKey(k => k.Id);
-                a.Property(p => p.Id).HasColumnName("Id");
-                a.Property(p => p.Name).HasColumnName("Name");
-            });
-
-            Brand[] brandEntitySeeds = { new(1, "BMW"), new(2, "Mercedes") };
-            modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+            
 
         }
        
