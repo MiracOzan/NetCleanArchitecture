97e87ddde38977188d47bf5066b6ddf13b27e180
Miraç Ozan T <m.ozan.tosun@gmail.com>
2022-09-07 23:47:41 +0300

    İmplements WebApi

---
+++ Application/Features/Models/Queries/GetListModelByDynamic/GetListModelByDynamicQuerry.cs
@@ -0,0 +1,42 @@
+﻿using Application.Features.Models.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Models.Queries.GetListModelByDynamic
+{
+    public class GetListModelByDynamicQuerry: IRequest<ModelListModel>
+    {
+        public Dynamic Dynamic { get; set; }
+        public PageRequest PageRequest { get; set; }
+
+        public class GetListModelByDynamicQuerryHandler : IRequestHandler<GetListModelByDynamicQuerry, ModelListModel>
+        {
+            private readonly IModelRepository _modelRepository;
+            private readonly IMapper _mapper;
+
+            public GetListModelByDynamicQuerryHandler(IModelRepository modelRepository, IMapper mapper)
+            {
+                _modelRepository = modelRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<ModelListModel> Handle(GetListModelByDynamicQuerry request, CancellationToken cancellationToken)
+            {
+                IPaginate<Model> models = await _modelRepository.GetListByDynamicAsync(dynamic:request.Dynamic,
+                    include:m => m.Include(c => c.Brand),
+                    index:request.PageRequest.Page,
+                    size:request.PageRequest.PageSize
+                );
+
+                ModelListModel mapperModels = _mapper.Map<ModelListModel>(models);
+                return mapperModels;
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/ModelsController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Models.Models;
+using Application.Features.Models.Queries;
+using Application.Features.Models.Queries.GetListModelByDynamic;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuerry getListModelsQuerry = new(){PageRequest = pageRequest};
+            ModelListModel result = await Mediator?.Send(getListModelsQuerry);
+            return Ok(result);
+        }
+
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListModelByDynamicQuerry getListModelsByDynamicQuerry = new(){PageRequest = pageRequest};
+            ModelListModel result = await Mediator?.Send(getListModelsByDynamicQuerry);
+            return Ok(result);
+        }
+    }
+}
